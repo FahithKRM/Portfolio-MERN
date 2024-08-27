2e0e2b5625861be7fc81d2b5290759d1a045fbf0
FahithKRM <krmfahith@gmail.com>
2024-08-27 19:19:39 +0530

    modified

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,19 @@
-import "./Header.css";
-import profile_img from "../../assets/image1.png";
-import CircumIcon from "@klarr-agency/circum-icons-react"; // React
-import AnchorLink from "react-anchor-link-smooth-scroll";
+// Header.js
+import './Header.css'
+import profile_img from '../../assets/image1.png';
+import CircumIcon from '@klarr-agency/circum-icons-react';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Header = () => {
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = 'https://drive.google.com/file/d/1o1Z-cUe3egugi5n3Ry4QbsYzlCcXwF8p/view?usp=drive_link';
+    link.download = 'FahithKRM_Resume.pdf'; // Name of the downloaded file
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <div id="home" className="header">
       <img src={profile_img} alt="" />
@@ -13,30 +23,32 @@ const Header = () => {
           Aspiring Computer Engineer with a Passion for Innovation
         </h2>
         <p className="para">
-          I am a dedicated computer engineering student at University of Jaffna.
+          I am a dedicated computer engineering student at the University of Jaffna.
           I specialize in software development and have a keen interest in
-          artificial intelligence, React Native and MERN stack.
+          artificial intelligence, React Native, and MERN stack.
         </p>
         <div className="header-action">
           <AnchorLink className="anchor-link" offset={50} href="#contact">
             <button className="header-connect">Connect with me</button>
           </AnchorLink>
-          <button className="header-resume">My resume</button>
+          <button className="header-resume" onClick={handleDownload}>
+            My resume
+          </button>
         </div>
         <div className="social-icons">
           <div className="social-icon">
             <a href="">
-            <CircumIcon name="facebook" size="36px" color="#fff" />
+              <CircumIcon name="facebook" size="36px" color="#fff" />
             </a>
           </div>
           <div className="social-icon">
             <a href="https://x.com/Faahith_KRM">
-            <CircumIcon name="twitter" size="36px" color="#fff" />
+              <CircumIcon name="twitter" size="36px" color="#fff" />
             </a>
           </div>
           <div className="social-icon">
             <a href="https://www.linkedin.com/in/krmfahith/">
-            <CircumIcon name="linkedin" size="36px" color="#fff" />
+              <CircumIcon name="linkedin" size="36px" color="#fff" />
             </a>
           </div>
         </div>
